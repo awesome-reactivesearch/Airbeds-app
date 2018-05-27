155d8ea4f371bbbe0c464d65a87e6d88886c6246
rajmeghpara <meghpararajkumar@gmail.com>
2018-05-27 19:24:53 +0530

    step2: setup with all components included from ReactiveSearch library

--- src/App.js
+++ src/App.js
@@ -1,19 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {
+  ReactiveBase,
+  DataSearch,
+  NumberBox,
+  DateRange,
+  RangeSlider
+} from "@appbaseio/reactivesearch";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactiveBase
+        app="housing"
+        credentials="0aL1X5Vts:1ee67be1-9195-4f4b-bd4f-a91cd1b5e4b5"
+        type="listing"
+        theme={{
+          colors: {
+            primaryColor: "#41ABF5"
+          }
+        }}
+      >
+        <DataSearch
+          componentId="search"
+          dataField="name"
+          autosuggest={false}
+          placeholder="Search housings..."
+          iconPosition="left"
+          className="search"
+        />
+        <RangeSlider
+          componentId="PriceSensor"
+          dataField="price"
+          title="Price Range"
+          range={{
+            start: 10,
+            end: 250
+          }}
+          rangeLabels={{
+            start: "$10",
+            end: "$250"
+          }}
+          defaultSelected={{
+            start: 10,
+            end: 50
+          }}
+          stepValue={10}
+          interval={20}
+          react={{
+            and: ["DateRangeSensor", "GuestSensor"]
+          }}
+          className="rangeFilter"
+        />
+        <NumberBox
+          componentId="GuestSensor"
+          dataField="accommodates"
+          title="Guests"
+          defaultSelected={2}
+          labelPosition="right"
+          data={{
+            start: 1,
+            end: 16
+          }}
+          className="numberFilter"
+        />
+        <DateRange
+          dataField="date_from"
+          componentId="DateRangeSensor"
+          title="When"
+          numberOfMonths={2}
+          queryFormat="basic_date"
+          initialMonth={new Date("04/01/2017")}
+          className="dateFilter"
+        />
+      </ReactiveBase>
     );
   }
 }
