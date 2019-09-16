0353ec79c36050eace7952d8a1b30dfdd7f6b6fc
anuj <anujshah584@gmail.com>
2019-09-16 14:57:41 +0530

    update: rs versions

--- src/App.js
+++ src/App.js
@@ -12,96 +12,96 @@ class App extends Component {
   render() {
     return (
       <div className="main-container">
-        <ReactiveBase
-          app="housing"
-          credentials="0aL1X5Vts:1ee67be1-9195-4f4b-bd4f-a91cd1b5e4b5"
-          type="listing"
-          theme={{
-            colors: {
-              primaryColor: "#41ABF5"
-            }
-          }}
-        >
-          <div className="nav-container">
-            <nav className="nav">
-              <div className="title">Airbeds</div>
-            </nav>
-          </div>
-          <div className="filters-search-container">
-            <div className="filter-container">
-              <div className="dropdown">
-                <button className="button">Price</button>
-                <div className="dropdown-content">
-                  <RangeSlider
-                    componentId="PriceSensor"
-                    dataField="price"
-                    title="Price Range"
-                    range={{
-                      start: 10,
-                      end: 250
-                    }}
-                    rangeLabels={{
-                      start: "$10",
-                      end: "$250"
-                    }}
-                    defaultSelected={{
-                      start: 10,
-                      end: 50
-                    }}
-                    stepValue={10}
-                    interval={20}
-                    react={{
-                      and: ["DateRangeSensor", "GuestSensor"]
-                    }}
-                    className="rangeFilter"
-                  />
-                </div>
+          <ReactiveBase
+              app="airbeds-test-app"
+              credentials="X8RsOu0Lp:9b4fe1a4-58c6-4089-a042-505d86d9da30"
+              type="listing"
+              theme={{
+                  colors: {
+                      primaryColor: "#41ABF5"
+                  }
+              }}
+          >
+              <div className="nav-container">
+                  <nav className="nav">
+                      <div className="title">Airbeds</div>
+                  </nav>
               </div>
-              <div className="dropdown">
-                <button className="button">Guests</button>
-                <div className="dropdown-content-guest">
-                  <NumberBox
-                    componentId="GuestSensor"
-                    dataField="accommodates"
-                    title="Guests"
-                    defaultSelected={2}
-                    labelPosition="right"
-                    data={{
-                      start: 1,
-                      end: 16
-                    }}
-                    className="numberFilter"
-                  />
-                </div>
-              </div>
+              <div className="filters-search-container">
+                  <div className="filter-container">
+                      <div className="dropdown">
+                          <button className="button">Price</button>
+                          <div className="dropdown-content">
+                              <RangeSlider
+                                  componentId="PriceSensor"
+                                  dataField="price"
+                                  title="Price Range"
+                                  range={{
+                                      start: 10,
+                                      end: 250
+                                  }}
+                                  rangeLabels={{
+                                      start: "$10",
+                                      end: "$250"
+                                  }}
+                                  defaultValue={{
+                                      start: 10,
+                                      end: 50
+                                  }}
+                                  stepValue={10}
+                                  interval={20}
+                                  react={{
+                                      and: ["DateRangeSensor", "GuestSensor"]
+                                  }}
+                                  className="rangeFilter"
+                              />
+                          </div>
+                      </div>
+                      <div className="dropdown">
+                          <button className="button">Guests</button>
+                          <div className="dropdown-content-guest">
+                              <NumberBox
+                                  componentId="GuestSensor"
+                                  dataField="accommodates"
+                                  title="Guests"
+                                  defaultValue={2}
+                                  labelPosition="right"
+                                  data={{
+                                      start: 1,
+                                      end: 16
+                                  }}
+                                  className="numberFilter"
+                              />
+                          </div>
+                      </div>
 
-              <div className="dropdown">
-                <button className="button ">When</button>
-                <div className="dropdown-content">
-                  <DateRange
-                    dataField="date_from"
-                    componentId="DateRangeSensor"
-                    title="When"
-                    numberOfMonths={2}
-                    queryFormat="basic_date"
-                    initialMonth={new Date("04/01/2017")}
-                    className="dateFilter"
-                  />
-                </div>
+                      <div className="dropdown">
+                          <button className="button ">When</button>
+                          <div className="dropdown-content">
+                              <DateRange
+                                  dataField="date_from"
+                                  componentId="DateRangeSensor"
+                                  title="When"
+                                  numberOfMonths={2}
+                                  queryFormat="basic_date"
+                                  initialMonth={new Date("04/01/2017")}
+                                  className="dateFilter"
+                              />
+                          </div>
+                      </div>
+                  </div>
+                  <div className="search-container">
+                      <DataSearch
+                          componentId="search"
+                          dataField="name"
+                          autosuggest={false}
+                          placeholder="Search housings..."
+                          iconPosition="left"
+                          className="search"
+                      />
+                  </div>
               </div>
-            </div>
-            <div className="search-container">
-              <DataSearch
-                componentId="search"
-                dataField="name"
-                autosuggest={false}
-                placeholder="Search housings..."
-                iconPosition="left"
-                className="search"
-              />
-            </div>
-          </div>
-        </ReactiveBase>
+          </ReactiveBase>
       </div>
     );
   }
