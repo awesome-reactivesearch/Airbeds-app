67863553ed1f6d8e4cb2f6a2b8f5a8dd375d0a09
anuj <anujshah584@gmail.com>
2020-04-14 17:50:10 +0530

    feat: migrate app to appbase-demo cluster

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import {
   ReactiveBase,
   DataSearch,
@@ -13,167 +13,168 @@ class App extends Component {
   render() {
     return (
       <div className="main-container">
-          <ReactiveBase
-              app="airbeds-test-app"
-              credentials="X8RsOu0Lp:9b4fe1a4-58c6-4089-a042-505d86d9da30"
-              type="listing"
-              theme={{
-                  colors: {
-                      primaryColor: "#41ABF5"
-                  }
-              }}
-          >
-              <div className="nav-container">
-                  <nav className="nav">
-                      <div className="title">Airbeds</div>
-                  </nav>
-              </div>
-              <div className="filters-search-container">
-                  <div className="filter-container">
-                      <div className="dropdown">
-                          <button className="button">Price</button>
-                          <div className="dropdown-content">
-                              <RangeSlider
-                                  componentId="PriceSensor"
-                                  dataField="price"
-                                  title="Price Range"
-                                  range={{
-                                      start: 10,
-                                      end: 250
-                                  }}
-                                  rangeLabels={{
-                                      start: "$10",
-                                      end: "$250"
-                                  }}
-                                  defaultValue={{
-                                      start: 10,
-                                      end: 50
-                                  }}
-                                  stepValue={10}
-                                  interval={20}
-                                  react={{
-                                      and: ["DateRangeSensor", "GuestSensor"]
-                                  }}
-                                  className="rangeFilter"
-                              />
-                          </div>
-                      </div>
-                      <div className="dropdown">
-                          <button className="button">Guests</button>
-                          <div className="dropdown-content-guest">
-                              <NumberBox
-                                  componentId="GuestSensor"
-                                  dataField="accommodates"
-                                  title="Guests"
-                                  defaultValue={2}
-                                  labelPosition="right"
-                                  data={{
-                                      start: 1,
-                                      end: 16
-                                  }}
-                                  className="numberFilter"
-                              />
-                          </div>
-                      </div>
-
-                      <div className="dropdown">
-                          <button className="button ">When</button>
-                          <div className="dropdown-content">
-                              <DateRange
-                                  dataField="date_from"
-                                  componentId="DateRangeSensor"
-                                  title="When"
-                                  numberOfMonths={2}
-                                  queryFormat="basic_date"
-                                  initialMonth={new Date("04/01/2017")}
-                                  className="dateFilter"
-                              />
-                          </div>
-                      </div>
-                  </div>
-                  <div className="search-container">
-                      <DataSearch
-                          componentId="search"
-                          dataField="name"
-                          autosuggest={false}
-                          placeholder="Search housings..."
-                          iconPosition="left"
-                          className="search"
-                      />
-                  </div>
-              </div>
-
-              <div className="result-map-container">
-                  <ReactiveGoogleMap
-                      componentId="map"
-                      dataField="location"
-                      defaultZoom={13}
-                      pagination
-                      onPageChange={() => {
-                          window.scrollTo(0, 0);
-                      }}
-                      style={{
-                          width: "calc(100% - 280px)",
-                          height: "calc(100vh - 52px)"
-                      }}
-                      className="rightCol"
-                      showMarkerClusters={false}
-                      showSearchAsMove={false}
-                      innerClass={{
-                          label: "label"
-                      }}
-                      renderAllData={(
-                          hits,
-                          streamHits,
-                          loadMore,
-                          renderMap,
-                          renderPagination
-                      ) => (
-                          <div style={{ display: "flex" }}>
-                              <div>
-                                  <div className="card-container">
-                                      {hits.map(data => (
-                                          <div key={data._id} className="card">
-                                              <div
-                                                  className="card__image"
-                                                  style={{ backgroundImage: `url(${data.image})` }}
-                                                  alt={data.name}
-                                              />
-                                              <div>
-                                                  <h2>{data.name}</h2>
-                                                  <div className="card__price">${data.price}</div>
-                                                  <p className="card__info">
-                                                      {data.room_type} · {data.accommodates} guests
-                                                  </p>
-                                              </div>
-                                          </div>
-                                      ))}
-                                  </div>
-                                  <div>{renderPagination()}</div>
-                              </div>
-                              <div className="map-container">{renderMap()}</div>
-                          </div>
-                      )}
-                      renderData={data => ({
-                          label: (
-                              <div
-                                  className="marker"
-                                  style={{
-                                      width: 40,
-                                      display: "block",
-                                      textAlign: "center"
-                                  }}
-                              >
-                                  <div className="extra-info">{data.name}</div>${data.price}
-                              </div>
-                          )
-                      })}
-                      react={{
-                          and: ["GuestSensor", "PriceSensor", "DateRangeSensor", "search"]
-                      }}
+        <ReactiveBase
+          app="airbeds-test-app"
+          url="https://xe6N9nDRV:51ea7a8a-6354-4b5f-83e1-12dce3b7ec47@arc-cluster-appbase-demo-ps1pgt.searchbase.io"
+          enableAppbase
+          type="listing"
+          theme={{
+            colors: {
+              primaryColor: "#41ABF5"
+            }
+          }}
+        >
+          <div className="nav-container">
+            <nav className="nav">
+              <div className="title">Airbeds</div>
+            </nav>
+          </div>
+          <div className="filters-search-container">
+            <div className="filter-container">
+              <div className="dropdown">
+                <button className="button">Price</button>
+                <div className="dropdown-content">
+                  <RangeSlider
+                    componentId="PriceSensor"
+                    dataField="price"
+                    title="Price Range"
+                    range={{
+                      start: 10,
+                      end: 250
+                    }}
+                    rangeLabels={{
+                      start: "$10",
+                      end: "$250"
+                    }}
+                    defaultValue={{
+                      start: 10,
+                      end: 50
+                    }}
+                    stepValue={10}
+                    interval={20}
+                    react={{
+                      and: ["DateRangeSensor", "GuestSensor"]
+                    }}
+                    className="rangeFilter"
                   />
+                </div>
               </div>
-          </ReactiveBase>
+              <div className="dropdown">
+                <button className="button">Guests</button>
+                <div className="dropdown-content-guest">
+                  <NumberBox
+                    componentId="GuestSensor"
+                    dataField="accommodates"
+                    title="Guests"
+                    defaultValue={2}
+                    labelPosition="right"
+                    data={{
+                      start: 1,
+                      end: 16
+                    }}
+                    className="numberFilter"
+                  />
+                </div>
+              </div>
+
+              <div className="dropdown">
+                <button className="button ">When</button>
+                <div className="dropdown-content">
+                  <DateRange
+                    dataField="date_from"
+                    componentId="DateRangeSensor"
+                    title="When"
+                    numberOfMonths={2}
+                    queryFormat="basic_date"
+                    initialMonth={new Date("04/01/2017")}
+                    className="dateFilter"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="search-container">
+              <DataSearch
+                componentId="search"
+                dataField="name"
+                autosuggest={false}
+                placeholder="Search housings..."
+                iconPosition="left"
+                className="search"
+              />
+            </div>
+          </div>
+
+          <div className="result-map-container">
+            <ReactiveGoogleMap
+              componentId="map"
+              dataField="location"
+              defaultZoom={13}
+              pagination
+              onPageChange={() => {
+                window.scrollTo(0, 0);
+              }}
+              style={{
+                width: "calc(100% - 280px)",
+                height: "calc(100vh - 52px)"
+              }}
+              className="rightCol"
+              showMarkerClusters={false}
+              showSearchAsMove={false}
+              innerClass={{
+                label: "label"
+              }}
+              renderAllData={(
+                hits,
+                streamHits,
+                loadMore,
+                renderMap,
+                renderPagination
+              ) => (
+                <div style={{ display: "flex" }}>
+                  <div>
+                    <div className="card-container">
+                      {hits.map(data => (
+                        <div key={data._id} className="card">
+                          <div
+                            className="card__image"
+                            style={{ backgroundImage: `url(${data.image})` }}
+                            alt={data.name}
+                          />
+                          <div>
+                            <h2>{data.name}</h2>
+                            <div className="card__price">${data.price}</div>
+                            <p className="card__info">
+                              {data.room_type} · {data.accommodates} guests
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                    <div>{renderPagination()}</div>
+                  </div>
+                  <div className="map-container">{renderMap()}</div>
+                </div>
+              )}
+              renderData={data => ({
+                label: (
+                  <div
+                    className="marker"
+                    style={{
+                      width: 40,
+                      display: "block",
+                      textAlign: "center"
+                    }}
+                  >
+                    <div className="extra-info">{data.name}</div>${data.price}
+                  </div>
+                )
+              })}
+              react={{
+                and: ["GuestSensor", "PriceSensor", "DateRangeSensor", "search"]
+              }}
+            />
+          </div>
+        </ReactiveBase>
       </div>
     );
   }
