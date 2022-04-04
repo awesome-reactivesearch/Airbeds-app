be3c626794dd95dd25339842cf6ce51fa5f370c2
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-04 11:58:21 +0530

    Add rangeslider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import {
+  RangeSlider,
   ReactiveBase, SearchBox
 } from "@appbaseio/reactivesearch";
 
@@ -19,6 +20,32 @@ class App extends Component {
             }
           }}
         >
+          <div className="dropdown-content">
+              <RangeSlider
+                componentId="PriceSensor"
+                dataField="price"
+                title="Price Range"
+                range={{
+                  start: 10,
+                  end: 250
+                }}
+                rangeLabels={{
+                  start: "$10",
+                  end: "$250"
+                }}
+                defaultValue={{
+                  start: 10,
+                  end: 50
+                }}
+                stepValue={10}
+                interval={20}
+                react={{
+                  and: ["DateRangeSensor", "GuestSensor"]
+                }}
+                showHistogram={false}
+                className="rangeFilter"
+              />
+          </div>
           <div className="search-container">
               <SearchBox
                 componentId="search"
