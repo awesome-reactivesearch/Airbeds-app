fc4f0289cb689e6daebf902279edb8d28a99193a
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-04 12:39:43 +0530

    Add map

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 } from "@appbaseio/reactivesearch";
 
 import './App.css'
+import { ReactiveGoogleMap } from "@appbaseio/reactivemaps";
 
 class App extends Component {
   render() {
@@ -103,6 +104,76 @@ class App extends Component {
                 className="search"
               />
             </div>
+          </div>
+          <div className="result-map-container">
+            <ReactiveGoogleMap
+              componentId="map"
+              dataField="location"
+              defaultZoom={13}
+              pagination
+              onPageChange={() => {
+                window.scrollTo(0, 0);
+              }}
+              style={{
+                width: "calc(100% - 280px)",
+                height: "calc(100vh - 52px)"
+              }}
+              className="rightCol"
+              showMarkerClusters={false}
+              showSearchAsMove={false}
+              innerClass={{
+                label: "label"
+              }}
+              renderAllData={(
+                hits,
+                streamHits,
+                loadMore,
+                renderMap,
+                renderPagination
+              ) => (
+                <div style={{ display: "flex" }}>
+                  <div>
+                    <div className="card-container">
+                      {hits.map(data => (
+                        <div key={data._id} className="card">
+                          <div
+                            className="card__image"
+                            style={{ backgroundImage: `url(${data.image})` }}
+                            alt={data.name}
+                          />
+                          <div>
+                            <h2>{data.name}</h2>
+                            <div className="card__price">${data.price}</div>
+                            <p className="card__info">
+                              {data.room_type} · {data.accommodates} guests
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                    <div>{renderPagination()}</div>
+                  </div>
+                  <div className="map-container">{renderMap()}</div>
+                </div>
+              )}
+              renderData={data => ({
+                label: (
+                  <div
+                    className="marker"
+                    style={{
+                      width: 40,
+                      display: "block",
+                      textAlign: "center"
+                    }}
+                  >
+                    <div className="extra-info">{data.name}</div>${data.price}
+                  </div>
+                )
+              })}
+              react={{
+                and: ["GuestSensor", "PriceSensor", "DateRangeSensor", "search"]
+              }}
+            />
           </div>
         </ReactiveBase>
       </div>
