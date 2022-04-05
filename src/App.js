76a2832eb7c8340826b945cd811b7ee528988987
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-05 10:52:56 +0530

    Hide future

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
 import {
-  RangeSlider,
   ReactiveBase,
-  SearchBox,
-  NumberBox,
-  DateRange
+  SearchBox
 } from "@appbaseio/reactivesearch";
-
-import './App.css'
-import { ReactiveGoogleMap } from "@appbaseio/reactivemaps";
 
 class App extends Component {
   render() {
