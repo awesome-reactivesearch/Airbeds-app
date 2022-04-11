bd4b583b1e7dfbec1381ab434a2bcf464fb4899a
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-11 12:52:06 +0530

    Remove unused packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createRoot} from 'react-dom/client';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = ()=> {
+    return (
+      <div className="main-container">
+        {/* Component that connects backend */}
+        <ReactiveBase
+          app="clone-airbeds"
+          url="https://73afb5484d0e:26bd5cb0-1afc-4e19-8870-4a2eda8d0b56@appbase-demo-ansible-abxiydt-arc.searchbase.io"
+          enableAppbase
+          theme={{
+            colors: {
+              primaryColor: "#41ABF5"
+            }
+          }}
+        >
+            Hello from Reactivesearch!
+        </ReactiveBase>
+      </div>
+    );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
